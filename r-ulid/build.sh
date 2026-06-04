3ac3445febbc800d9b329c7b5d44ba7323fbae84
pdiakumis <peterdiakumis@gmail.com>
2026-06-04 11:42:05 +1000

    try use native ubuntu-24.04-arm for linux-aarch64 builds

--- r-ulid/build.sh
+++ r-ulid/build.sh
@@ -2,10 +2,5 @@
 
 export DISABLE_AUTOBREW=1
 
-# cross-r-base leaves SHLIB_LIBADD undefined; define it in user Makevars so
-# R's install code doesn't get a length-zero vector from nzchar()
-mkdir -p "${HOME}/.R"
-echo "SHLIB_LIBADD = " >> "${HOME}/.R/Makevars"
-
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
